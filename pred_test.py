2bf9545d81468e0f021600a6d3e269c764a1dd42
Wei Zeng <81786865+BolshevikGuard@users.noreply.github.com>
2025-02-19 16:34:19 +0800

    config json

--- pred_test.py
+++ pred_test.py
@@ -1,17 +1,22 @@
+import json
 from ultralytics import YOLO
 
+# 读取JSON配置文件
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
 # 加载训练好的模型
-model = YOLO("runs/detect/yolov8n_test/weights/last.pt")
+model = YOLO(config["model"])
 
 # 对文件夹进行推理
 results = model.predict(
-    source="my_test",  # 输入文件夹路径
-    save=True,
-    iou=0.3,
-    save_txt=True,
-    save_conf=True,
-    conf=0.5,
-    project="my_test_output",  # 指定输出父目录
-    name="detection_results",  # 指定输出子目录
-    exist_ok=True  # 允许覆盖已有目录
+    source=config["source"],  # 输入文件夹路径
+    save=config["save"],
+    iou=config["iou"],
+    save_txt=config["save_txt"],
+    save_conf=config["save_conf"],
+    conf=config["conf"],
+    project=config["project"],  # 指定输出父目录
+    name=config["name"],  # 指定输出子目录
+    exist_ok=config["exist_ok"]  # 允许覆盖已有目录
 )
