45783fd0e3870b54774ce49c2469fa5cda501602
Wei Zeng <81786865+BolshevikGuard@users.noreply.github.com>
2025-03-11 13:54:56 +0800

    Update and rename host.py to client.py

--- http_json/client.py
+++ http_json/client.py
@@ -5,11 +5,12 @@ PORT = 12345  # 端口号，与服务器端保持一致
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((SERVER_IP, PORT))
-    print("已连接到服务器，输入消息发送，输入 'exit' 退出")
+    print("server connected")
 
     while True:
-        message = input("请输入消息: ")
+        print("[s] take one round pics / [e] exit")
+        message = input()
         client_socket.sendall(message.encode())
         if message.strip().lower() == "exit":
-            print("发送退出命令，断开连接")
+            print("disconnected!")
             break
